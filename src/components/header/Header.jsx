02322ea77af1b496916320901202a4cdbf67f294
sri766 <srisanthseth28@gmail.com>
2023-12-23 01:06:29 +0530

    complete with new icon change2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/movix-logo.svg";
 
 const Header = () => {
     const [show, setShow] = useState("top");
